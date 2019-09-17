3db57b089b3ec9aa01d0b06d9284abf788ed63b7
AlexMorgos <morgos.alexander@gmail.com>
2019-09-17 13:54:11 +0300

    sum-of-differences-in-array

---
+++ arrays/arraystasks.js
@@ -0,0 +1,9 @@
+function sum_adjacent_difference(arr) {
+	var result = 0;
+	for (var i = 1; i < arr.length; i++) {
+		result += Math.abs(arr[i] - arr[i - 1]);
+	}
+	return result;
+}
+
+console.log(sum_adjacent_difference([5, 3, 8, 1, -10]));
